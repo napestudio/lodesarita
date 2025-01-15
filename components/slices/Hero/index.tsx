8d267d3538f347b0fb8e3ed44a26cd63aa1204f4
Renzo Costarelli <renzo.costarelli@gmail.com>
2025-01-15 19:01:11 -0300

    Modifica animacion hero para desktop

--- components/slices/Hero/index.tsx
+++ components/slices/Hero/index.tsx
@@ -4,10 +4,9 @@ import { PrismicNextImage } from "@prismicio/next";
 import { SliceComponentProps } from "@prismicio/react";
 import { useEffect, useLayoutEffect, useRef, useState } from "react";
 import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+
 import { Flip } from "gsap/Flip";
 import "./hero.css";
-import Link from "next/link";
 
 gsap.registerPlugin(Flip);
 
@@ -45,22 +44,18 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
             Flip.from(state, {
               duration: 1,
               ease: "power1.inOut",
-              // absolute: true,
             });
             Flip.from(videoState, {
               duration: 1,
               ease: "power1.inOut",
-              // absolute: true,
             });
             Flip.from(videoSmallState, {
               duration: 1,
               ease: "power1.inOut",
-              // absolute: true,
             });
             Flip.from(textRefState, {
               duration: 1,
               ease: "power1.inOut",
-              // absolute: true,
               onComplete: () => {
                 const textRefState = Flip.getState(textRef.current);
                 const videoState = Flip.getState(videoWrapperRef.current);
@@ -69,7 +64,7 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
                 gsap.to("[data-paragraph-big] [data-line]", {
                   y: 150,
                   stagger: 0.15,
-                  duration: 0.5,
+                  duration: 1,
                   ease: "power1.inOut",
                   delay: 1,
                 });
@@ -91,27 +86,20 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
                   // absolute: true,
                   delay: 2,
                 });
-                gsap.to("[data-paragraph]", {
-                  opacity: 1,
-                  delay: 2,
-                });
-                gsap.to("[data-paragraph] [data-line]", {
-                  y: 0,
-                  stagger: 0.2,
-                  duration: 1,
-                  ease: "power1.inOut",
-                  delay: 1,
-                });
               },
             });
           },
         })
-        .to("[data-paragraph-big] [data-line]", {
-          y: 0,
-          stagger: 0.2,
-          duration: 1,
-          ease: "power1.inOut",
-        });
+        .to(
+          "[data-paragraph-big] [data-line]",
+          {
+            y: 0,
+            stagger: 0.2,
+            duration: 1,
+            ease: "power1.inOut",
+          },
+          "0.5"
+        );
 
       tl.play();
     }, [heroRef.current]);
@@ -145,7 +133,7 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
           />
         </div>
         <div
-          className="w-full rounded-3xl overflow-hidden col-start-1 col-end-9 row-start-5 row-span-2"
+          className="w-full rounded-3xl overflow-hidden col-start-1 col-end-10 row-start-5 row-span-2"
           ref={video2WrapperRef}
           data-video-small
         >
@@ -161,7 +149,7 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
         </div>
         <div className="px-2 text-yellow w-full row-start-5 col-start-1 col-end-10">
           <div
-            className="text-3xl 2xl:text-5xl leading-none text-left flex flex-col"
+            className="text-3xl 2xl:text-6xl leading-none text-left flex flex-col"
             data-paragraph-big
           >
             <span className="overflow-hidden">
@@ -202,33 +190,23 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
           </div>
         </div>
         <div
-          className="col-start-10 col-end-13 w-full text-yellow flex flex-col justify-between"
+          className="col-start-10 col-end-13 row-end-7 row-start-5 w-full text-yellow flex flex-col justify-between overflow-hidden"
           ref={textRef}
-          data-text
+          data-logo-area
         >
           <PrismicNextImage
             field={slice.primary.imagen}
-            className="opacity-0 w-5/6 mx-auto"
+            className=" mx-auto"
             data-logo
           />
           <div className="text-white w-full pt-2">
-            <div
-              className="opacity-0 flex flex-col gap-5 justify-between"
-              data-paragraph
-            >
-              {/* create call to action button */}
-              <Link
-                href="#help"
-                className="w-full text-center py-2 bg-green-dark rounded-xl relative font-text font-bold text-yellow"
-              >
-                Habitaciones
-              </Link>
-              <a
-                href="#"
+            <div className="flex flex-col gap-5 justify-between">
+              <button
                 className="w-full text-center py-3 bg-yellow rounded-xl relative font-text font-bold text-green-dark"
+                data-cta
               >
                 Reservar
-              </a>
+              </button>
             </div>
           </div>
         </div>
