5d1b932fdc403f7d230e19a5140cc5f50777daeb
napestudio <holasomosnap@gmail.com>
2025-01-15 10:20:43 -0300

    feat: added dynamic page

---
+++ app/[uid]/page.tsx
@@ -0,0 +1,44 @@
+import { Metadata } from "next";
+import { notFound } from "next/navigation";
+import { SliceZone } from "@prismicio/react";
+
+import { createClient } from "@/prismicio";
+import { components } from "@/components/slices";
+
+type Params = { uid: string };
+
+export default async function Page({ params }: { params: Promise<Params> }) {
+  const { uid } = await params;
+  const client = createClient();
+  const page = await client.getByUID("room", uid).catch(() => notFound());
+
+  return (
+    <div>
+      <SliceZone slices={page.data.slices} components={components} />
+    </div>
+  );
+}
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<Params>;
+}): Promise<Metadata> {
+  const { uid } = await params;
+  const client = createClient();
+  const page = await client.getByUID("room", uid).catch(() => notFound());
+
+  return {
+    title: page.data.meta_title,
+    description: page.data.meta_description,
+  };
+}
+
+export async function generateStaticParams() {
+  const client = createClient();
+  const pages = await client.getAllByType("room");
+
+  return pages.map((page) => {
+    return { uid: page.uid };
+  });
+}
