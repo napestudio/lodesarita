05bb1eda065e11848e4b5a78e6a78037d0c89e82
cuerojr <cannibal5033@gmail.com>
2025-01-21 14:12:40 -0300

    feat: minor fix

--- components/slices/Rooms/index.tsx
+++ components/slices/Rooms/index.tsx
@@ -1,4 +1,4 @@
-import { Content } from "@prismicio/client";
+import { Content, ImageFieldImage } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
 import { cms } from "@/prismicio";
 import { PrismicNextImage } from "@prismicio/next";
@@ -14,8 +14,7 @@ export type RoomsProps = SliceComponentProps<Content.RoomsSlice>;
  * Component for "Rooms" Slices.
  */
 export default async function Rooms({ slice }: RoomsProps) {
-  const rooms = await cms.getAllByType("room");
-  //console.log("🚀 ~ Rooms ~ rooms:", rooms)
+  const rooms = (await cms.getAllByType("room"))?.reverse();
 
   return (
     <section className="py-16 md:py-24">
@@ -35,36 +34,36 @@ export default async function Rooms({ slice }: RoomsProps) {
                   field={room.data.slices[0].primary.miniatura}
                 />
               </div>
-              <div className="flex flex-col justify-between md:min-h-[12rem]">
-                <div className="flex justify-between gap-4 items-start p-4">
-                  <div>
-                    <h3 className="text-2xl font-bold mb-2">
-                      {room.data.slices[0].primary.titulo}
-                    </h3>
-                    <p className="text-muted-foreground font-text font-semibold line-clamp-3">
-                      {room.data.slices[0].primary.descripcion}
-                    </p>
+              <div className="flex justify-between gap-4 items-start p-4">
+                <div>
+                  <h3 className="text-2xl font-bold mb-2">
+                    {room.data.slices[0].primary.titulo}
+                  </h3>
+                  <ul className="mb-3 flex justify-start gap-2">
+                    {room.data.slices[0].primary.caracteristicas.map(
+                      (item: any, i: number) => (
+                        <li key={i} className="flex items-center w-4">
+                          <PrismicNextImage
+                            field={item.icon}
+                            width={item.icon.dimensions?.width}
+                            height={item.icon.dimensions?.height}
+                          />
+                        </li>
+                      )
+                    )}
+                  </ul>
+                  <p className="text-muted-foreground font-text font-semibold line-clamp-3">
+                    {room.data.slices[0].primary.descripcion}
+                  </p>
+                </div>
+                <div className="flex flex-col items-end">
+                  <div className="text-3xl font-text font-bold leading-[1]">
+                    $200
                   </div>
-                  <div className="flex flex-col items-end">
-                    <div className="text-3xl font-text font-bold leading-[1]">$200</div>
-                    <div className="text-sm text-muted-foreground font-text font-semibold leading-[1]">
-                      por noche
-                    </div>
+                  <div className="text-sm text-muted-foreground font-text font-semibold leading-[1]">
+                    por noche
                   </div>
                 </div>
-                <ul className="p-3 flex justify-start gap-2">
-                  {room.data.slices[0].primary.caracteristicas.map(
-                    (item: any | null, index: number) => (
-                      <li key={index} className="flex items-center w-4">
-                        <PrismicNextImage
-                          field={item.icon}
-                          width={item.icon.dimensions?.width}
-                          height={item.icon.dimensions?.height}
-                        />
-                      </li>
-                    )
-                  )}
-                </ul>
               </div>
             </TransitionLink>
           ))}
