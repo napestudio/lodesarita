c7aa63cd1a7892b6671cc2038e7339faafd05d62
cuerojr <cannibal5033@gmail.com>
2025-03-11 12:39:04 -0300

    wip: refactor

--- components/maps/index.tsx
+++ components/maps/index.tsx
@@ -1,22 +1,15 @@
-import { memo, useState } from "react";
+import { useMemo } from "react";
 import {
   MapContainer,
   TileLayer,
   Marker,
   useMap,
   ZoomControl,
-  Popup,
 } from "react-leaflet";
 import { Icon, LatLngLiteral } from "leaflet";
 import "leaflet/dist/leaflet.css";
-import Image from "next/image";
-import { PrismicNextImage } from "@prismicio/next";
-import { useGlobal } from "@/lib/store";
 
-type MapType = "roadmap" | "satellite" | "hybrid" | "terrain";
-
-type MapLocation = LatLngLiteral & { id: string };
-
+type MapLocation = LatLngLiteral;
 type MapProps = { center: LatLngLiteral; locations: MapLocation[] };
 
 const SelectedLocation = ({ center }: { center: LatLngLiteral }) => {
@@ -25,48 +18,15 @@ const SelectedLocation = ({ center }: { center: LatLngLiteral }) => {
   return null;
 };
 
-export const Map: React.FC<MapProps> = memo(({ center, locations }) => {
-  const { openModalWithSelection } = useGlobal();
-  const [mapType, setMapType] = useState<MapType>("terrain");
-  const [selectedLocation, setSelectedLocation] = useState<
-    MapLocation | undefined
-  >();
-
-  const getUrl = () => {
-    const mapTypeUrls: Record<MapType, string> = {
-      roadmap: "http://mt0.google.com/vt/lyrs=m&hl=en&x={x}&y={y}&z={z}",
-      satellite: "http://mt0.google.com/vt/lyrs=s&hl=en&x={x}&y={y}&z={z}",
-      hybrid: "http://mt0.google.com/vt/lyrs=y&hl=en&x={x}&y={y}&z={z}",
-      terrain: "http://mt0.google.com/vt/lyrs=p&hl=en&x={x}&y={y}&z={z}",
-    };
-    return mapTypeUrls[mapType];
-  };
-
-  const mapMarkIcon = new Icon({
-    iconUrl: "map-marker.png",
-    iconSize: [37, 50],
-  });
-  const mapMarkActiveIcon = new Icon({
-    iconUrl: "active-map-marker.png",
-    iconSize: [57, 65],
-  });
-
-  const renderMarks = () => {
-    return locations.map((location) => (
-      <div key={location.id}>
-        <Marker
-          icon={
-            location.id === selectedLocation?.id
-              ? mapMarkActiveIcon
-              : mapMarkIcon
-          }
-          position={{ lat: location.lat, lng: location.lng }}          
-        >
-          
-        </Marker>
-      </div>
-    ));
-  };
+export default function Map({ center, locations }: MapProps) {
+  const mapMarkIcon = useMemo(
+    () =>
+      new Icon({
+        iconUrl: "map-marker.png",
+        iconSize: [37, 50],
+      }),
+    []
+  );
 
   return (
     <div className="container h-[20rem] bg-green overflow-hidden">
@@ -78,11 +38,16 @@ export const Map: React.FC<MapProps> = memo(({ center, locations }) => {
         attributionControl={false}
         style={{ width: "100%", height: "100%", borderRadius: ".75rem" }}
       >
-        <TileLayer url={getUrl()} />
-        {selectedLocation && <SelectedLocation center={selectedLocation} />}
-        {renderMarks()}
+        <TileLayer url="http://mt0.google.com/vt/lyrs=p&hl=en&x={x}&y={y}&z={z}" />
+        {locations.map((location, i) => (
+          <Marker
+            key={i}
+            icon={mapMarkIcon}
+            position={{ lat: location.lat, lng: location.lng }}
+          />
+        ))}
         <ZoomControl position="topright" />
       </MapContainer>
     </div>
   );
-});
+}
--- components/maps/map.tsx
+++ components/maps/map.tsx
@@ -1,22 +1,10 @@
 "use client";
-import React from "react";
 import dynamic from "next/dynamic";
 
-const Map = dynamic(
-  () => import("@/components/maps").then((component) => component.Map),
-  { ssr: false }
-);
+const Map = dynamic(() => import("@/components/maps"), { ssr: false });
 
 export default function MapComponent() {
-  const locations = [
-    {
-      id: "550e8400-e29b-41d4-a716-446655440000",
-      lng: -58.72845526984698,
-      lat: -38.5811825920464,
-    },
-  ];
-  
-  return (
-    <Map center={{ lng: -58.72858, lat: -38.58116 }} locations={locations} />
-  );
+  const locations = [{ lng: -58.72858, lat: -38.58116 }];
+
+  return <Map center={{ lng: -58.72858, lat: -38.58116 }} locations={locations} />;
 }
